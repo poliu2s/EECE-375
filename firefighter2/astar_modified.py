b982799e4cef5eafe6e02472b98032289086e789
Po Liu <po.liu.2s@gmail.com>
2013-05-10 15:14:07 -0700

    Initial Commit

---
+++ firefighter2/astar_modified.py
@@ -0,0 +1,287 @@
+# EECE375/474 Team 15
+# Robot Implementation
+# A* Pathfinding Algorithm
+#
+# Pathfinding adapted from:
+# http://code.activestate.com/recipes/577519-a-star-shortest-path-algorithm/
+#
+# Copyright (C) 2011 by Jake Davis, Po Liu, Anuj Mehta, Vincent Quach, Yuyang Sun, Ian Tu
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# IMPORT DECLARATIONS
+from heapq import heappush, heappop # for priority queue
+import math
+import time
+import imageProcessing
+import random
+from candle import Candle
+from path import Path
+import os
+import Global_Constants
+
+# GLOBAL VARIABLES
+candle_list = []
+# for i in [0,1,2,3,4,5,6,7]:
+    # candle_list.append(Candle(random.randrange(15, 45, 1),
+                              # random.randrange(15, 45, 1),
+                              # "Black", 1, 0))
+the_map = Global_Constants.map_global
+n = Global_Constants.n_global
+m = Global_Constants.m_global
+dx = Global_Constants.dx_global
+dy = Global_Constants.dy_global
+dirs = Global_Constants.dirs_global
+step = Global_Constants.step_global 
+
+##########
+# A*STAR #
+##########
+
+class node:
+    xPos = 0 # x position
+    yPos = 0 # y position
+    distance = 0 # total distance already travelled to reach the node
+    priority = 0 # priority = distance + remaining distance estimate
+    def __init__(self, xPos, yPos, distance, priority):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.distance = distance
+        self.priority = priority
+    def __lt__(self, other): # comparison method for priority queue
+        return self.priority < other.priority
+    def updatePriority(self, xDest, yDest):
+        self.priority = self.distance + self.estimate(xDest, yDest) * 10 # A*
+    # give higher priority to going straight instead of diagonally
+    def nextMove(self, dirs, d): # d: direction to move
+        if dirs == 8 and d % 2 != 0:
+            self.distance += 19
+        else:
+            self.distance += 10
+    # Estimation function for the remaining distance to the goal.
+    def estimate(self, xDest, yDest):
+        xd = xDest - self.xPos
+        yd = yDest - self.yPos
+        d = math.sqrt(xd * xd + yd * yd)
+        return(d)
+
+        
+        
+class PathFinder:
+
+    def __init__(self):
+        t = time.time()
+        row = [0] * n
+        for i in range(m): # create empty map
+                the_map.append(list(row))
+        
+    # generalize is a function that takes a path result and discretizes it
+    # further into a set of distances and angles that can be sent to the
+    # robot
+    def generalize(self, initx, inity, robot_angle, path):
+        turns = 0
+        segments = []
+        angle = int(path[0]) * 45 - robot_angle
+        print 'Angle: ', angle
+        distance = 0
+        segment = ''
+
+        segments.append(Path(initx,inity, robot_angle, distance * math.cos(robot_angle), distance * math.sin(robot_angle), angle, segment))
+
+        # for each node step in the path (type: str)
+        for i in range(len(path)):
+
+            distance += step * math.sqrt(int(path[i]) % 2 + 1)
+            
+            # if the current element of the path is not equal to the next
+            # one, that means we can anticipate a turn. Calculate our
+            # current angle, the next angle, take the difference, and
+            # perform some operations to gain the relative angle to turn
+            if i == (len(path) - 1):
+                turns += 1
+                seg = Path(initx, inity, robot_angle, distance * math.cos(robot_angle), distance * math.sin(robot_angle), angle, 1)
+                segments.append(seg)
+                print 'Distance: ', distance
+                print 'Angle: ', angle
+            else:
+                if path[i] != path[i+1]:
+                    turns += 1
+                    curr_angle = int(path[i]) * 45 # current angle of path
+                    next_angle = int(path[i+1]) * 45 # next angle of path
+                    diff_angle = next_angle - curr_angle # difference in angles
+                    if(abs(diff_angle) < 180):
+                        angle = diff_angle
+                    else:
+                        if(diff_angle < 0):
+                            angle += 360
+                        else:
+                            angle -= 360
+                            seg = Path(initx, inity, robot_angle, distance * math.cos(robot_angle), distance * math.sin(robot_angle), angle, 0)
+                            segments.append(seg)
+                    print 'Distance: ', distance
+                    print 'Angle: ', angle
+                    distance = 0
+
+        print 'Turns: ', turns
+        print 'Time (seconds):', time.time() - t
+        return segments
+
+    # target() simply determines which of the four corners of a buffered
+    # candle the robot should go to. There is a destination, but it is
+    # inaccessible due to the surrounding buffer created within the
+    # place_obstacles function. Depending on the robot's position, one of the
+    # four corners will be chosen as the destination.
+    def target(self, xA, yA, candle):
+        xB = -1 if (xA - candle.getX() < 0) else 1
+        yB = -1 if (yA - candle.getY() < 0) else 1
+        xB = xB * 2 + candle.getX()
+        yB = yB * 2 + candle.getY()
+        print 'Candle X: {0} \nCandle Y:{1}\nRobot X: {2}\nRobot Y:{3}'.format(candle.getX(), candle.getY(), xA, yA)
+        return xB, yB
+
+    # place_obstacles takes in a list of candle objects and 
+    def place_obstacles(self, candleList):
+        for candle in candleList:
+            the_map[candle.getX()][candle.getY()] = 1
+            for ddx,ddy in ((-1,-1), (-1, 0), (-1, 1), (0, -1),
+                            ( 0, 1), (1, -1), ( 1, 0), ( 1, 1),
+                            (0, -2), (1, -2), (-1,-2),     # left side
+                            ( 0, 2), ( 1, 2), (-1, 2),     # right side
+                            ( 2, 1), ( 2, 0), (2, -1),     # bottom side
+                            (-2, 1), (-2, 0), (-2,-1)):    # top side
+                the_map[candle.getX() + ddx][candle.getY() + ddy] = 1
+
+    # A-star algorithm.
+    # The path returned will be a string of digits of directions.
+    def pathFind(self, xA, yA, xB, yB):
+
+        closed_nodes_map = [] # map of closed (tried-out) nodes
+        open_nodes_map = [] # map of open (not-yet-tried) nodes
+        dir_map = [] # map of dirs
+        # checkpoint = [yA][xA] # start point of a path
+        row = [0] * n
+        for i in range(m): # create 2d arrays
+            closed_nodes_map.append(list(row))
+            open_nodes_map.append(list(row))
+            dir_map.append(list(row))
+
+        pq = [[], []] # priority queues of open (not-yet-tried) nodes
+        pqi = 0 # priority queue index
+        # create the start node and push into list of open nodes
+        n0 = node(xA, yA, 0, 0)
+        n0.updatePriority(xB, yB)
+        heappush(pq[pqi], n0)
+        open_nodes_map[yA][xA] = n0.priority # mark it on the open nodes map
+
+        # A* search
+        while len(pq[pqi]) > 0:
+            # get the current node w/ the highest priority
+            # from the list of open nodes
+            n1 = pq[pqi][0] # top node
+            n0 = node(n1.xPos, n1.yPos, n1.distance, n1.priority)
+            # nextpoint = [n1.yPos][n1.xPos] # specify nextpoint for walkable()
+            x = n0.xPos
+            y = n0.yPos
+            heappop(pq[pqi]) # remove the node from the open list
+            open_nodes_map[y][x] = 0
+            closed_nodes_map[y][x] = 1 # mark it on the closed nodes map
+
+            # quit searching when the goal is reached
+            # if n0.estimate(xB, yB) == 0:
+            if x == xB and y == yB:
+                # generate the path from finish to start
+                # by following the dirs
+                path = ''
+                while not (x == xA and y == yA):
+                    j = dir_map[y][x]
+                    c = str((j + dirs / 2) % dirs)
+                    path = c + path
+                    x += dx[j]
+                    y += dy[j]
+                return path
+
+            # generate moves (child nodes) in all possible dirs
+            for i in range(dirs):
+                xdx = x + dx[i]
+                ydy = y + dy[i]
+                if not (xdx < 0 or xdx > n-1 or ydy < 0 or ydy > m - 1
+                        or the_map[ydy][xdx] == 1 or closed_nodes_map[ydy][xdx] == 1):
+                    # generate a child node
+                    m0 = node(xdx, ydy, n0.distance, n0.priority)
+                    m0.nextMove(dirs, i)
+                    m0.updatePriority(xB, yB)
+                    # if it is not in the open list then add into that
+                    if open_nodes_map[ydy][xdx] == 0:
+                        open_nodes_map[ydy][xdx] = m0.priority
+                        heappush(pq[pqi], m0)
+                        # mark its parent node direction
+                        dir_map[ydy][xdx] = (i + dirs / 2) % dirs
+                    elif open_nodes_map[ydy][xdx] > m0.priority:
+                        # update the priority
+                        open_nodes_map[ydy][xdx] = m0.priority
+                        # update the parent direction
+                        dir_map[ydy][xdx] = (i + dirs / 2) % dirs
+                        # replace the node
+                        # by emptying one pq to the other one
+                        # except the node to be replaced will be ignored
+                        # and the new node will be pushed in instead
+                        while not (pq[pqi][0].xPos == xdx and pq[pqi][0].yPos == ydy):
+                            heappush(pq[1 - pqi], pq[pqi][0])
+                            heappop(pq[pqi])
+                        heappop(pq[pqi]) # remove the target node
+                        # empty the larger size priority queue to the smaller one
+                        if len(pq[pqi]) > len(pq[1 - pqi]):
+                            pqi = 1 - pqi
+                        while len(pq[pqi]) > 0:
+                            heappush(pq[1-pqi], pq[pqi][0])
+                            heappop(pq[pqi])
+                        pqi = 1 - pqi
+                        heappush(pq[pqi], m0) # add the better node instead
+        return '' # if no route found
+
+        
+    def printMap(self):
+        # mark the route on the map
+        if len(route) > 0:
+            x = xA
+            y = yA
+            the_map[y][x] = 2
+            for i in range(len(route)):
+                j = int(route[i])
+                x += dx[j]
+                y += dy[j]
+                the_map[y][x] = 3
+            the_map[y][x] = 4
+        # display the map with the route added
+        print 'Map:'
+        for y in range(m):
+            for x in range(n):
+                xy = the_map[y][x]
+                if xy == 0:
+                    print '.', # space
+                elif xy == 1:
+                    print 'O', # obstacle
+                elif xy == 2:
+                    print 'S', # start
+                elif xy == 3:
+                    print '&', # route
+                elif xy == 4:
+                    print 'F', # finish
+            print
+        print 'Route:', route
---
+++ firefighter2/controller.py
@@ -0,0 +1,301 @@
+# TODO: Exit condition
+# TODO: Send entire path rather than path segments to MCU at one go.
+
+import threading
+import math
+#import serial
+import time
+import sys
+import robot
+import candle
+import path
+import os
+import Global_Constants
+from imageProcessing import ecePredator
+from corner_detection import eceCorners
+from astar_modified import PathFinder
+
+def findCheckSum(distance) :
+    total = 0
+    while distance != 0: 
+        total += distance%10
+        distance = int(distance/10)
+    return int(total%10)
+
+def verifyCheckSum(gyro_data):
+    total = gyro_data % 10
+    sum = 0
+    while gyro_data != 0:
+        gyro_data = int(gyro_data / 10)
+        sum += gyro_data % 10
+    if sum == total:
+        return true
+    return false
+
+def convertToSendable(data):
+    data = str(data)
+    if len(data) == 5:
+        return str(data)
+    elif len(data) < 5:
+        while len(data) < 5:
+            data = "0" + data
+        return data
+    
+def addToOutgoingMessage(message):
+    if self.outgoing_message == "00000":
+        self.outgoing_message = message
+    else:
+        self.outgoing_message += message
+    
+# take picture and assign it name as given
+# TODO: Change this eventually (add -i /dev/video1)
+def takePicture(name):
+    os.system("streamer -o "+name)
+
+def getNextPictureName(number):
+    return (Global_Constants.IMAGE_BASENAME + str(number) + Global_Constants.IMAGE_EXT)
+
+def getPositionFromDisplacement(old_x, old_y, distance, angle):
+    new_x = old_x + distance * math.cos(angle)
+    new_y = old_y + distance * math.sin(angle)
+    return (new_x, new_y)
+    
+# Call this function to run the algorithm. 
+def getNextBestPathList(our_candle_list, attack_candle_list, robot):
+    path_finder = astar()
+    min_path_list = []
+    min_distance = 0 
+    min_number_segments = 0
+    # Place obstacles on the field.
+    # Find a way to place obstacle robot as an obstacle in case of "ALERT"
+    path_finder.place_obstacles(our_candle_list)
+    path_finder.place_obstacles(attack_candle_list)
+    # Iterate through every candle that is left to be blown out in the attack_candle_list
+    for candle in attack_candle_list : 
+        current_candle_status = candle.getStatus()
+        if current_candle_status == 0:
+            # temp_path_list = path_finder.getAttackAlgorithm()
+            x_robot = robot.getX()
+            y_robot = robot.getY()
+            angle_robot = robot.getCurrentAngle()
+            # Find the "destination point" which is slightly different from the candle location
+            x_dest, y_dest = path_finder.target(x_robot, y_robot, candle)
+            route = path_finder.pathFind(x_robot, y_robot, x_dest, y_dest)
+            temp_path_list = path_finder.generalize(x_robot, y_robot, angle_robot, route)
+            if min_number_segments == 0 and min_distance == 0:
+                min_path_list = temp_path_list
+                min_number_segments = len(temp_path_list)
+                for temp_path in temp_path_list:
+                    min_distance += temp_path.getDistance()
+            elif len(temp_path_list) < min_number_segments:
+                min_path_list = temp_path_list
+                min_number_segments = len(temp_path_list)
+                min_distance = 0
+                for temp_path in temp_path_list:
+                    min_distance += temp_path.getDistance()
+            elif len(temp_path_list) == min_number_segments:
+                compare_distance = 0
+                for temp_path in temp_path_list: 
+                    compare_distance += temp_path.getDistance()
+                if compare_distance < min_distance:
+                    min_distance = compare_distance
+                    min_path_list = temp_path_list
+    return min_path_list
+    
+# Get candle coordinates from image processing, store them locally.
+black_candle_list = []
+white_candle_list = []
+
+# Configure/Calibrate the camera
+# camera.configure()
+number_pictures = 0
+name = getNextPictureName(number_pictures)
+number_pictures += 1
+
+corner_finder = eceCorners()
+candle_finder = ecePredator()
+#takePicture(name)
+corners = corner_finder.findCorners(name)
+# Load the most recently taken picture into candle_finder.
+candle_finder.loadPicture(name)
+# Once picture is loaded, its corners can be used to warp the image
+candle_finder.warpPicture(corners)
+# Once picture is warped, it can be used to find all the candles on the field.
+print "Finding black candles"
+white_candle_list = candle_finder.findWhiteCandles()
+print "Finding white candles"
+black_candle_list = candle_finder.findBlackCandles()
+
+# Also find the initial position of the robot.
+
+# This for loop is only to print candle locations. 
+for black_candle in black_candle_list:
+    x = black_candle.getX
+    y = black_candle.getY
+    status = black_candle.getStatus
+    color = black_candle.getColor
+    print "{0}, {1}, {2}, {3}".format(str(x()), str(y()), str(status()), str(color()))
+    #print str(x()) + " , " + str(y()) + " , " + str(status()) + " , " + str(color())
+
+# This for loop is only to print candle locations. 
+for white_candle in white_candle_list:
+    x = white_candle.getX
+    y = white_candle.getY
+    status = white_candle.getStatus
+    color = white_candle.getColor
+    print "{0}, {1}, {2}, {3}".format(str(x()), str(y()), str(status()), str(color()))
+    #print str(x()) + " , " + str(y()) + " , " + str(status()) + " , " + str(color())
+
+
+# Check the number of candles found. If the number is greater than expected, throw exception and restart image processing. Perhaps have some mechanism in image processing to ensure that only 4 are returned?
+if len(black_candle_list) != Global_Constants.NUMBER_OF_BLACK_CANDLES or len(white_candle_list) != Global_Constants.NUMBER_OF_WHITE_CANDLES: 
+    print "Error: Unexpected number of candles on field"
+    print "Number of black candles found: {0}".format(len(black_candle_list))
+    print "Number of white candles found: {0}".format(len(white_candle_list))
+    exit()
+
+
+attack_candle_list = black_candle_list
+our_candle_list = white_candle_list
+if( Global_Constants.CANDLES_TO_ATTACK == 1):
+    attack_candle_list = white_candle_list
+    our_candle_list = black_candle_list
+
+# Keep track of current state by keeping a robot and a current_path object
+bamf = robot(0, 0, 0)
+# Keep track of path finding algorithm with a list of paths.
+current_path_list = []
+
+# Then send instructions to the robot and start listening on the XBee.
+# Send instructions to MCU over XBee
+try:
+    # TODO: Change port while switching over to Unix.
+    ser = serial.Serial(port='\\.\COM4')
+    ser.flushInput()
+    ser.flushOutput()
+    ser.close()
+    ser.baudrate = 115200
+    ser.timeout = 1
+    ser.open()
+except: 
+    print "Could not initiate serial communication. Terminating program."
+    sys.exit()
+
+# Start a thread to receive incoming messages and put them in a queue
+self.outgoing_message = "00000"
+last_outgoing_message = "00000"
+last_request = "00000"
+while True:
+    if self.outgoing_message != "00000":
+        last_outgoing_message = self.outgoing_message[:5]
+        self.outgoing_message = self.outgoing_message[5:]
+        ser.write(last_outgoing_message)
+        if len(self.outgoing_message) == 0:
+            self.outgoing_message = "00000"
+        continue
+    if ser.inWaiting >= 5:
+        data = ser.read(5)
+        if data == "FIRST":
+            # This means that its the first time the MCU sent a request to the controller.
+            # Get 5 more bytes for location of robot.
+            # Initialise robot position to those 5 bytes.
+            # Initialise path finding algorithm. 
+            # Send distance to move to MCU.
+            print "Received 'FIRST' from the MCU"
+            while ser.inWaiting < 5:
+                continue
+            distance_moved_data = ser.read(5)
+            distance_moved = distance_moved_data[:-1] if verifyCheckSum(distance_moved_data) else 0
+            old_x = bamf.getX()
+            old_y = bamf.getY()
+            # Here I'm assuming angle should be zero, ie robot is moving in x direction. What if our orientation is different?
+            new_x, new_y = getPositionFromDisplacement(old_x, old_y, distance_moved, 0)
+            bamf.updatePosition(new_x, new_y)
+            current_path_list = getNextBestPathList( our_candle_list, attack_candle_list, bamf)
+            current_path = current_path_list.pop(0)
+            addToOutgoingMessage(convertToSendable(current_path.getDistance()))
+            addToOutgoingMessage(convertToSendable(current_path.getCurrentAngle()))
+            addToOutgoingMessage(convertToSendable(current_path.getToCandle()))
+            last_request = data
+        elif data == "RECVD":
+            print "Received 'RECVD' from the MCU"
+            last_outgoing_message = "00000"
+            last_request = data
+            # This means instructions were received.
+        elif data == "ALERT":
+            print "Received 'ALERT' from the MCU"
+            # ? Get obstacle distance information from IR sensor ? 
+            # Take picture. Run pathfinding algorithm to get new algorithm
+            name = getNextPictureName(number_pictures)
+            number_pictures += 1
+            takePicture(name)
+            # robot_finder = eceRobot()
+            # robot_finder.loadPicture(name)
+            # robot_finder.warpPicture(corners)
+            # robot_finder.getRobotPosition(bamf)
+            # Add an robot obstacle to the algorithm finder thing
+            # Do algorithm stuff
+            last_request = data
+            
+        elif data == "TRACK":
+            # Receive five more bytes of data and then create new thread to store it
+            # Store this in robot object
+            print "Tracking readings are being received now"
+            last_request = data
+            while ser.inWaiting < 5:
+                continue
+            track_data = ser.read(5)
+            if verifyCheckSum(track_data):
+                new_x += track_data *  math.cos(bamf.getCurrentAngle())
+                new_y += track_data * math.sin(bamf.getCurrentAngle())
+                bamf.updatePosition(new_x, new_y)
+        elif data == "RQDIS":
+            # Since robot is asking for distance, it has finished the most recent part of the segment.
+            # Check if there are any more parts of the segment left. If not, set that candle's status to extinguished. Generate new algorithm for next candles. 
+            # If there are more segments left, move on to the next part of the path_list and send that distance to the MCU. 
+            # Before sending message, make sure you assign a checksum to it. 
+            last_request = data
+            if current_path_list != 0:
+                current_path = current_path_list.pop(0)
+                addToOutgoingMessage(convertToSendable(current_path.getDistance()))
+            print "Sending distance instruction to MCU"
+        elif data == "RQANG":
+            # Send the angle to turn to the robot to get the end of the next segment
+            # Ensure angle is checksumed. 
+            last_request = data
+            addToOutgoingMessage(convertToSendable(current_path.getDistance()))
+            print "Sending angle to MCU"
+        elif data == "GYROS":
+            # Listen for five more bytes of data from the serial port. Create new thread to store the angle information
+            # Store this in robot.
+            print "Gyroscope readings being received now"
+            while ser.inWaiting < 5:
+                continue
+            gyro_data = ser.read(5)
+            if verifyCheckSum(gyro_data):
+                # Assuming angles are given +/-
+                new_angle += bamf.getCurrentAngle()
+                bamf.updateAngle(new_angle)
+        elif data == "CANDL":
+            # Return whether there's a candle at the end of the current_path or not
+            print "Request received for CANDL"
+            if current_path.getToCandle() == 0:
+                addToOutgoingMessage(convertToSendable("0"))
+            else:
+                addToOutgoingMessage(convertToSendable("1"))
+        elif data == "RPEAT":
+            # Resend last sent message
+            if last_request == "RQANG" or last_request == "RQDIS" or last_request == "CANDL":
+                addToOutgoingMessage(last_outgoing_message)
+            elif last_request == "ALERT" or last_request == "FIRST":
+                addToOutgoingMessage(convertToSendable(current_path.getDistance()))
+                addToOutgoingMessage(convertToSendable(current_path.getCurrentAngle()))
+                addToOutgoingMessage(convertToSendable(current_path.getToCandle()))
+            print "Request received for resending data"
+        elif data == "DONEX":
+            # Kill the loop
+            print "Received Done- Killing loop"
+            break;
+        else:
+            # Unrecognized request received. Send a resend request. 
+            print "Random data received - " + data
